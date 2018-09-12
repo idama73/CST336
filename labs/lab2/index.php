8102d416b60047ff4afb6719b69021d1e9e8fa49
Arnold Okumagba <idamaokumagba@rocketmail.com>
2018-09-12 06:19:13 +0000

    updating file

---
+++ labs/lab2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+        
+        <?php
+        
+        $random_value = rand(0,2); //generate a random number from 0 to 2
+        
+        echo $random_value;
+        
+        $symbol ="orange";
+        
+
+        echo "<img src ='img/$symbol.png'alt='$symbol' title='.ucfirst($symbol)'/>";
+        
+        ?>
+        
+        
+      
+        
+     
+
+
+    </body>
+</html>
