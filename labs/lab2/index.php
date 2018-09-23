9459d39db0928ed8b0c5595a7a84aa2c842c847b
Arnold Okumagba <idamaokumagba@rocketmail.com>
2018-09-23 03:22:34 +0000

    updating file

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>777 Slot Machine </title>
+        <title> 777 Slot Machine </title>
         <style>
             @import url("css/styles.css");
         </style>
@@ -27,7 +27,20 @@
         </form>
         
         </div>
-       
+      <footer>
+            <hr>
+            
+            <center> 
+         
+             &COPY; 2018 Okumagba <br />
+            <strong>Disclaimer: </strong> the information in this webpage is fictitous, it is used for academic purposes only. <br />
+           
+               
+                <img src = "../../img/csumb_logo.jpg" width = "120"alt ="CSUMB Logo" />
+                                                
+                                                <img src = "../../img/buddy.jpg" width ="80"alt ="buddy program Logo" />
+</center>
+               </footer>
 
     </body>
 </html>
