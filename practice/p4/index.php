65a9400a2200dabbe5167627f9e2b47e47368ea4
Arnold Okumagba <idamaokumagba@rocketmail.com>
2018-10-03 01:19:11 +0000

    updating file

---
+++ practice/p4/index.php
@@ -0,0 +1,18 @@
+<div class ="container">
+            <div>
+                <center>
+                <p>Aces vs Kings</p>
+                
+                
+                <form action="playHand.php" method="POST">
+                    <div>
+                        <label>Number of Rows:</label><input type="number" name="p1" />
+                        <div />
+                     <div>
+                         <label>Number of columns:</label><input type="text" name="p2" />
+                         <div />
+                      
+                    
+                    <input type="submit" value="Submit" />
+        </center>
+                </form> 
