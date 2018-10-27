3d7c85efb45f3a66700470f21f84452833f32778
Arnold Okumagba <idamaokumagba@rocketmail.com>
2018-10-27 22:52:06 +0000

    file

--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -114,6 +114,20 @@ function filterProducts() {
         <?= filterProducts() ?>
         
     
+      <footer>
+            <hr>
+            
+            <center> 
+         
+             &COPY; 2018 Okumagba <br />
+            <strong>Disclaimer: </strong> the information in this webpage is fictitous, it is used for academic purposes only. <br />
+           
+               
+                <img src = "../../img/csumb_logo.jpg" width = "120"alt ="CSUMB Logo" />
+                                                
+                                                <img src = "../../img/buddy.jpg" width ="80"alt ="buddy program Logo" />
+</center>
+               </footer>
 
 
     </body>
