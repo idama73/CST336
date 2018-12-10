84ac812b7faf0bc9cdeae258e065359c0844e7d7
Arnold Okumagba <idamaokumagba@rocketmail.com>
2018-12-10 20:28:36 +0000

    update

--- labs/lab10/inc/footer.php
+++ labs/lab10/inc/footer.php
@@ -2,7 +2,7 @@
         <footer>
             
             Disclaimer: The information on this site is fake.<br>
-            &copy; Lara 2019
+            &copy; Okumagba 2019
             
         </footer>
         
